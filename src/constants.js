3bda0763b7ac823df5b0ab18746e5d5c9598f209
Diego Fernando Castro <dfernando.castro@outlook.com>
2026-03-24 22:29:53 -0300

    ---

--- src/constants.js
+++ src/constants.js
@@ -3,7 +3,7 @@ window.CONSTANTS = {
         CRIADORES_PUBLICOS: '../db.json',
         CRIADORES_ANIMALES: 'https://merino-desa.codes.com.ar:8445/handlers/CriadoresAnimales.ashx',
         AUTH_LOGON2: 'https://merino-desa.codes.com.ar:8445/handlers/logon.ashx',
-        AUTH_LOGON : 'http://localhost:6992/handlers/logon.ashx',
+        AUTH_LOGON : 'https://merino-desa.codes.com.ar:8445/handlers/logon.ashx',
         ANIMAL_DETALLE: 'https://merino-desa.codes.com.ar:8445/handlers/AnimalesConsultaPublica.ashx'
     }
 };
